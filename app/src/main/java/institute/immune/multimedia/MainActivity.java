d2b9d04d8806183743dcbedc941fd659cb2dca13
Forby02 <adrian.barbeito@immune.institute>
2022-02-01 12:24:16 +0100

    He introducido funcionalidad para escuchar musica

--- app/src/main/java/institute/immune/multimedia/MainActivity.java
+++ app/src/main/java/institute/immune/multimedia/MainActivity.java
@@ -2,14 +2,38 @@ package institute.immune.multimedia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main)
-          
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.paradise);
+    }
+
+    public void onClickPlay(View view) {
+        Toast toast = Toast.makeText(this, "Reproduciendo", Toast.LENGTH_LONG);
+        mediaPlayer.start();
+        toast.show();
+    }
+
+    public void onClickPause(View view) {
+        Toast toast = Toast.makeText(this, "Pausado", Toast.LENGTH_LONG);
+        toast.show();
+        mediaPlayer.pause();
+    }
+
+    public void onClickStop(View view) {
+        Toast toast = Toast.makeText(this, "Detenido", Toast.LENGTH_LONG);
+        toast.show();
+        mediaPlayer.stop();
+        mediaPlayer = MediaPlayer.create(this, R.raw.paradise);
     }
 }
